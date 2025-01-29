fbd8a40a66d331e0943fe3c8af1e49b05a8725f3
NgBlaze <okolaaprecious@gmail.com>
2025-01-29 21:32:51 +0100

    updated github_url

--- api/index.php
+++ api/index.php
@@ -6,7 +6,7 @@ header('Access-Control-Allow-Origin: *');
 $response = array(
     "email" => "okolaaprecious@gmail.com",
     "current_datetime" => gmdate("Y-m-d\TH:i:s\Z"),
-    "github_url" => "https://github.com/"
+    "github_url" => "https://github.com/NgBlaze/HNG12-BE-Stage-0"
 );
 
 
